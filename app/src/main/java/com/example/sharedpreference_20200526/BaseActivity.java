643663cffde1170f6ef9435230e70e90d1acd698
weekyeon <weekyeon@gmail.com>
2020-05-26 10:42:52 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sharedpreference_20200526/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.example.sharedpreference_20200526;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
+    public abstract void setUpEvents();
+    public abstract void setValues();
+
+}
---
+++ app/src/main/java/com/example/sharedpreference_20200526/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.sharedpreference_20200526;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.sharedpreference_20200526.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setUpEvents();
+        setValues();
+    }
+
+    @Override
+    public void setUpEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
